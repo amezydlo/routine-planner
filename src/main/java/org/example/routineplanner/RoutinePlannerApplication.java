c28e9d1f02ac51f52adc931871d229b3f0534841
Adam Mężydło <adamkmezydlo@gmail.com>
2024-02-19 14:04:35 +0100

    initial commit

---
+++ src/main/java/org/example/routineplanner/RoutinePlannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.routineplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutinePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/routineplanner/RoutinePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.routineplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
