6543753c77a71543c5b0e57ce3ca2a21489245aa
Adam Mężydło <adamkmezydlo@gmail.com>
2024-02-24 02:07:40 +0100

    add path prefix

---
+++ src/main/java/org/example/routineplanner/configuration/ApiPathConfig.java
@@ -0,0 +1,15 @@
+package org.example.routineplanner.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiPathConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/routine_planner/api", c -> c.isAnnotationPresent(RequestMapping.class));
+    }
+}
---
+++ src/main/java/org/example/routineplanner/controller/RoutineBlockController.java
@@ -0,0 +1,8 @@
+package org.example.routineplanner.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("")
+public class RoutineBlockController {
+}
